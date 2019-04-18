f404e7f258231b7fe0487bf621e82c8901d35908
Jean Barriere <jean@barriere.io>
2019-04-18 11:59:31 +0200

    feat(messenger): add messenger blocking

--- js/bengui.js
+++ js/bengui.js
@@ -1,99 +1,136 @@
-
 var bengui = function () {
-	var self = this;
+  var self = this
 
-	this.usernames = [ ];
-	this.posts = true;
-	this.comments = true;
-	this.timestep = 1000 * 1;
-	this.update = new updateOptions();
+  this.usernames = []
+  this.posts = true
+  this.comments = true
+  this.messenger = true
+  this.timestep = 1000 * 1
+  this.update = new updateOptions()
 
-	this.removeComments = function () {
-		var comments_block = document.querySelectorAll('.uiUfi');
+  this.removeMessengerConversation = function () {
+    var messenger_list = document.querySelector('.uiScrollableAreaContent').querySelectorAll('li')
+    var messages = document.querySelector('[aria-label=Messages]')
+    messenger_list.forEach((conversation, i) => {
+      const link = conversation.querySelector('a[data-href]')
+      const name = link.querySelector('[data-tooltip-content]').dataset.tooltipContent
+      const username = link.dataset.href.split('?')[0].split('/').reverse()[0]
+      if (self.usernames.includes(username)) {
+        conversation.parentNode.removeChild(conversation)
+        messages.querySelectorAll(`div[data-tooltip-content="${name}"]`).forEach((message, i) => {
+          const messageContainer = message.parentNode.parentNode.parentNode
+          messageContainer.parentNode.removeChild(messageContainer)
+        })
+      }
+    })
+    // for (i in messenger_list) {
+    // 	var conversation = messenger_list.get(i)
+    // 	console.log(Object.keys(conversation))
 
-		for (i in comments_block) {
-			var comment_block = comments_block[i];
-			var comment_list = comment_block.firstChild ? comment_block.firstChild.children : null;
+    // 	const username = conversation.baseURI.
+    // 	if (self.usernames.includes(username)) {
+    // 		conversation.style.display = 'none'
+    // 	}
+    // }
+  }
 
-			for (j in comment_list) {
-				var link_profile = comment_list[j];
+  this.removeComments = function () {
+    var comments_block = document.querySelectorAll('.uiUfi')
 
-				if (typeof link_profile == "object") {
-					var hrefProfile = link_profile.querySelector('.UFIImageBlockImage');
+    for (i in comments_block) {
+      var comment_block = comments_block[i]
+      var comment_list = comment_block.firstChild ? comment_block.firstChild.children : null
 
-					if (hrefProfile == null)
-						continue ;
-					hrefProfile = hrefProfile.getAttribute("href");
-					if (hrefProfile == null)
-						continue ;
-					hrefProfile = hrefProfile.split("?")[0].split('/').reverse()[0];
-					if (self.usernames.indexOf(hrefProfile) !== -1) {
-						comment_block.firstChild.removeChild(comment_list[j]);
-					}
-				}
-			}
-		}
-	}
+      for (j in comment_list) {
+        var link_profile = comment_list[j]
 
-	this.removePosts = function () {
-		var posts_block = document.querySelectorAll('._4-u2.mbm');
-		
-		for (i in posts_block) {
-			var post_block = posts_block[i];
+        if (typeof link_profile === 'object') {
+          var hrefProfile = link_profile.querySelector('.UFIImageBlockImage')
 
-			if (typeof post_block == "object") {
-				var hrefProfile = post_block.querySelector('.clearfix._5x46 > a');
+          if (hrefProfile == null) {
+            continue
+          }
+          hrefProfile = hrefProfile.getAttribute('href')
+          if (hrefProfile == null) {
+            continue
+          }
+          hrefProfile = hrefProfile.split('?')[0].split('/').reverse()[0]
+          if (self.usernames.indexOf(hrefProfile) !== -1) {
+            comment_block.firstChild.removeChild(comment_list[j])
+          }
+        }
+      }
+    }
+  }
 
-				if (hrefProfile == null)
-					continue ;
-				hrefProfile = hrefProfile.getAttribute("href");
-				if (hrefProfile == null)
-					continue ;
-				hrefProfile = hrefProfile.split("?")[0].split('/').reverse()[0];
-				if (self.usernames.indexOf(hrefProfile) !== -1) {
-					post_block.parentNode.removeChild(post_block);
-				}
-			}
-		}
-	}
+  this.removePosts = function () {
+    var posts_block = document.querySelectorAll('._4-u2.mbm')
 
-	this.updateOptions = function (changes, areaName) {
-		if (changes["comments"] !== undefined) {
-			self.comments = changes["comments"].newValue;
-		}
-		if (changes["posts"] !== undefined) {
-			self.posts = changes["posts"].newValue;
-		}
-		if (changes["username"] !== undefined) {
-			self.usernames = changes["username"].newValue.split(';');
-		}
-	}
+    for (i in posts_block) {
+      var post_block = posts_block[i]
 
-	this.check = function () {
-		if (self.comments) {
-			self.removeComments();
-		}
-		if (self.posts) {
-			self.removePosts();
-		}
-	}
+      if (typeof post_block === 'object') {
+        var hrefProfile = post_block.querySelector('.clearfix._5x46 > a')
 
-	this.update.getUsernames(function (usernames) {
-		self.usernames = usernames;
-	});
+        if (hrefProfile == null) {
+          continue
+        }
+        hrefProfile = hrefProfile.getAttribute('href')
+        if (hrefProfile == null) {
+          continue
+        }
+        hrefProfile = hrefProfile.split('?')[0].split('/').reverse()[0]
+        if (self.usernames.indexOf(hrefProfile) !== -1) {
+          post_block.parentNode.removeChild(post_block)
+        }
+      }
+    }
+  }
 
-	this.update.getPostsState(function (state) {
-		self.posts = state;
-	});
+  this.updateOptions = function (changes, areaName) {
+    if (changes['comments'] !== undefined) {
+      self.comments = changes['comments'].newValue
+    }
+    if (changes['posts'] !== undefined) {
+      self.posts = changes['posts'].newValue
+    }
+    if (changes['username'] !== undefined) {
+      self.usernames = changes['username'].newValue.split(';')
+    }
+  }
 
-	this.update.getCommentsState(function (state) {
-		self.comments = state;
-	});
-	setInterval(this.check, this.timestep);
+  this.check = function () {
+    if (self.comments) {
+      self.removeComments()
+    }
+    if (self.messenger) {
+      self.removeMessengerConversation()
+    }
+    if (self.posts) {
+      self.removePosts()
+    }
+  }
+
+  this.update.getUsernames(function (usernames) {
+    self.usernames = usernames
+  })
+
+  this.update.getPostsState(function (state) {
+    self.posts = state
+  })
+
+  this.update.getCommentsState(function (state) {
+    self.comments = state
+  })
+
+  this.update.getMessengerState(function (state) {
+    self.messenger = state
+  })
+  setInterval(this.check, this.timestep)
 }
 
 $(document).ready(function () {
-	var cleaner = new bengui();
+  var cleaner = new bengui()
 
-	chrome.storage.onChanged.addListener(cleaner.updateOptions);
-});
+  chrome.storage.onChanged.addListener(cleaner.updateOptions)
+})
--- js/updateOptions.js
+++ js/updateOptions.js
@@ -1,89 +1,129 @@
 var updateOptions = function () {
-	var self = this;
+  var self = this
 
-	this.usernames = [ ];
+  this.usernames = []
 
-	this.init = function () {
-		this.getUsernames(function (usernames) {
-			self.updatePersons();
-		});
-		this.getPostsState(function (state) {
-			$('#post-enabled').attr('checked', state);
-		});
-		this.getCommentsState(function (state) {
-			$('#comment-enabled').attr('checked', state);
-		});
-	}
+  this.init = function () {
+    this.getUsernames(function (usernames) {
+      self.updatePersons()
+    })
+    this.getPostsState(function (state) {
+      $('#post-enabled').attr('checked', state)
+    })
+    this.getCommentsState(function (state) {
+      $('#comment-enabled').attr('checked', state)
+    })
+    this.getMessengerState(function (state) {
+      $('#messenger-enabled').attr('checked', state)
+    })
+  }
 
-	this.updatePersons = function () {
-		var persons = $('#blocked-persons');
+  this.updatePersons = function () {
+    var persons = $('#blocked-persons')
 
-		persons.empty();
-		for (i in this.usernames) {
-			persons.append($('<li/>', { 'class' : 'list-group-item', 'text' : this.usernames[i] })
-				   .append($('<span/>', { 'class' :'badge', 'text' : 'X'})));
-		}
-	};
+    persons.empty()
+    for (i in this.usernames) {
+      persons.append($('<li/>', {
+        'class': 'list-group-item',
+        'text': this.usernames[i]
+      })
+        .append($('<span/>', {
+          'class': 'badge',
+          'text': 'X'
+        })))
+    }
+  }
 
-	this.getUsernames = function (callback) {
-		chrome.storage.sync.get('username', function (items) {
-			if (items["username"] && items["username"].length)
-				self.usernames = items["username"].split(';');
-			if (callback)
-				callback(self.usernames);
-		});
-	};
+  this.getUsernames = function (callback) {
+    chrome.storage.sync.get('username', function (items) {
+      if (items['username'] && items['username'].length) {
+        self.usernames = items['username'].split(';')
+      }
+      if (callback) {
+        callback(self.usernames)
+      }
+    })
+  }
 
-	this.setUsernames = function (callback) {
-		chrome.storage.sync.set({ 'username' : self.usernames.join(";") }, function () {
-			if (callback)
-				callback();
-		});
-	};
+  this.setUsernames = function (callback) {
+    chrome.storage.sync.set({
+      'username': self.usernames.join(';')
+    }, function () {
+      if (callback) {
+        callback()
+      }
+    })
+  }
 
-	this.addUsername = function (newUser, finish) {
-		var idx = this.usernames.indexOf(newUser);
+  this.addUsername = function (newUser, finish) {
+    var idx = this.usernames.indexOf(newUser)
 
-		if (idx === -1) {
-			this.usernames.push(newUser);
-			this.setUsernames(finish);
-		}
-	};
+    if (idx === -1) {
+      this.usernames.push(newUser)
+      this.setUsernames(finish)
+    }
+  }
 
-	this.removeUsername = function (toRemove, finish) {
-		var idx = this.usernames.indexOf(toRemove);
+  this.removeUsername = function (toRemove, finish) {
+    var idx = this.usernames.indexOf(toRemove)
 
-		if (idx !== -1) {
-			this.usernames.splice(idx, 1);
-			this.setUsernames(finish);
-		}
-	};
+    if (idx !== -1) {
+      this.usernames.splice(idx, 1)
+      this.setUsernames(finish)
+    }
+  }
 
-	this.setPostsState = function (state, callback) {
-		chrome.storage.sync.set({ 'posts' : state }, function () {
-			if (callback)
-				callback();
-		});
-	};
+  this.setPostsState = function (state, callback) {
+    chrome.storage.sync.set({
+      'posts': state
+    }, function () {
+      if (callback) {
+        callback()
+      }
+    })
+  }
 
-	this.setCommentsState = function (state, callback) {
-		chrome.storage.sync.set({ 'comments' : state }, function () {
-			if (callback)
-				callback();
-		});
-	};
+  this.setCommentsState = function (state, callback) {
+    chrome.storage.sync.set({
+      'comments': state
+    }, function () {
+      if (callback) {
+        callback()
+      }
+    })
+  }
 
-	this.getPostsState = function (callback) {
-		chrome.storage.sync.get('posts', function (items) {
-			if (items['posts'] !== undefined && callback)
-				callback(items["posts"]);
-		});
-	};
+  this.setMessengerState = function (state, callback) {
+    chrome.storage.sync.set({
+      'messenger': state
+    }, function () {
+      if (callback) {
+        callback()
+      }
+    })
+  }
 
-	this.getCommentsState = function (callback) {
-		chrome.storage.sync.get('comments', function (items) {
-			if (items['comments'] !== undefined && callback)
-				callback(items["comments"]);
-		});
-	};
+  this.getPostsState = function (callback) {
+    chrome.storage.sync.get('posts', function (items) {
+      if (items['posts'] !== undefined && callback) {
+        callback(items['posts'])
+      }
+    })
+  }
+
+  this.getCommentsState = function (callback) {
+    chrome.storage.sync.get('comments', function (items) {
+      if (items['comments'] !== undefined && callback) {
+        callback(items['comments'])
+      }
+    })
+  }
+
+  this.getMessengerState = function (callback) {
+    chrome.storage.sync.get('messenger', function (items) {
+      if (items['messenger'] !== undefined && callback) {
+        callback(items['messenger'])
+      }
+    })
+  }
 }
